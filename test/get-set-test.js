d04fc569466162ad3fbb31ccb596461552e80e07
wltsmrz <wltsmrz@gmail.com>
2015-01-30 13:04:34 -0800

    Cleanup

--- test/get-set-test.js
+++ test/get-set-test.js
@@ -1,30 +1,27 @@
-var assert = require("assert");
+var assert = require('assert');
 var expect = require('chai').expect;
 
 describe('test-get-set', function() {
-
-  var Knex     = require('knex');
-  var knex     = require('knex')({client: "sqlite3", connection: { "filename": "test.db" } });
+  var knex = require('knex')({ client: 'sqlite3', connection: { 'filename': ':memory:' } });
   var bookshelf = require('bookshelf')(knex);
   var plugin = require('../lib/encrypted-fields.js');
-  bookshelf.plugin(plugin, {key: 'abcd'});
   var TestModel = null;
 
+  bookshelf.plugin(plugin, { key: 'abcd' });
+
   before(function(done) {
-    knex.schema.dropTable('test')
-    .createTable('test', function (table) {
+    knex.schema.dropTableIfExists('test')
+    .createTable('test', function(table) {
       table.increments();
       table.string('name');
       table.string('important');
       table.timestamps();
-
     }).then(function() {
       TestModel = bookshelf.Model.extend({
         tableName: 'test'
       });
       done();
     });
-
   });
 
   beforeEach(function(done) {
@@ -33,7 +30,7 @@ describe('test-get-set', function() {
   })
 
   it('should return undefined for a get on an empty field', function(done) {
-    var m = TestModel.forge({name: 'abc'});
+    var m = TestModel.forge({ name: 'abc' });
     expect(m.get('important')).to.equal(undefined);
     expect(m.getEncrypted('important')).to.equal(undefined);
     done();
@@ -47,12 +44,9 @@ describe('test-get-set', function() {
   });
 
   it('should return the same value when encrypting and decrypting', function(done) {
-    var m = TestModel.forge({name: 'abc'});
+    var m = TestModel.forge({ name: 'abc' });
     m.setEncrypted('important', 'toot');
     expect(m.getEncrypted('important')).to.equal('toot');
     done();
   });
-
-
-
 });
